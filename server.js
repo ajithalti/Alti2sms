6b25bb31f11217b45ae2c57baac6cefc658be1e4
ajithalti <55635822+ajithalti@users.noreply.github.com>
2020-12-25 13:59:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(BodyParser.urlencoded({ extended: true }))
 var MongoClient = require('mongodb').MongoClient;
 //Create a database named "mydb":
 var url = "mongodb://localhost:27017/";
-
+console.log("aa");
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database created!");
